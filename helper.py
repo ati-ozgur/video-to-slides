42968a6245a736d4dcf621c781476edc7f9d7f71
Atilla Oezguer <ati.ozgur@gmail.com>
2024-04-04 12:34:59 +0300

    image similarity works

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import cv2
+from skimage.metrics import structural_similarity as ssim
 
 def video_to_images(video_path,output_dir):
 
@@ -21,13 +22,14 @@ def video_to_images(video_path,output_dir):
 		frame_number = int(frame_number)
 		filename = f"{output_dir}/frame_{frame_number:05d}.png"  # Pad with zeros
 
-		similarity = compare_histograms(prev_image,frame)
+		similarity = image_compare_structural_similarity(prev_image,frame)
+		prev_image = frame
 		if similarity > 0.9:
-			print("skipping saving image,similarity is high",similarity)
+			print("skipping saving image,similarity is high",similarity,"frame_number",frame_number)
 			continue
 
 		cv2.imwrite(filename, frame)
-		print("frame_number saved",frame_number)
+		print("frame_number saved",frame_number,"similarity",similarity)
 
 	cap.release()
 	cv2.destroyAllWindows()
@@ -44,3 +46,12 @@ def compare_histograms(image1, image2):
 	hist2 = cv2.calcHist([image2], [0], None, [256], [0, 256])
 	return cv2.compareHist(hist1, hist2, cv2.HISTCMP_CHISQR)  # Chi-squared metric
 
+def image_compare_structural_similarity(image1,image2):
+	if image1 is None or image2 is None:
+		return -1
+	image1_gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+	image2_gray = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
+	# Calculate and print SSIM score
+	ssim_value = ssim(image1_gray, image2_gray)
+	return ssim_value

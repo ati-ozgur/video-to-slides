75cf60d40c07425a442089feaa37cb251edc0aad
Atilla Oezguer <ati.ozgur@gmail.com>
2024-04-04 22:12:13 +0300

    help is added

--- video_to_images.py
+++ video_to_images.py
@@ -4,8 +4,8 @@ import argparse
 from helper import video_to_images
 
 parser = argparse.ArgumentParser()
-parser.add_argument('video_filename')   # positional argument
-parser.add_argument('-o', '--output_directory',default='extracted_frames')
+parser.add_argument('video_filename',help='video filename to load')   # positional argument
+parser.add_argument('-o', '--output_directory',default='extracted_frames',help='output directory to save png images files extracted from video')
 
 
 

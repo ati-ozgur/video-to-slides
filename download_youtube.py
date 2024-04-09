b6dcff3eb00a99c3c602eb93ea2de47f11c0b834
Atilla Oezguer <ati.ozgur@gmail.com>
2024-04-09 13:42:08 +0300

    extract functions to helper

--- download_youtube.py
+++ download_youtube.py
@@ -3,51 +3,7 @@ import re
 
 import argparse
 
-def get_video_id(url):
-	video_id_match = re.search(r"v=([^&]+)", url)
-	if not video_id_match:
-		raise ValueError("Failed to extract video ID from URL")
-	video_id = video_id_match.group(1)
-	return video_id
-
-
-
-def download_youtube_video(youtube_url, output_dir="videos"):
-	"""Downloads a YouTube video using yt-dlp (youtube-dl fork) and saves it with a unique filename.
-
-	Args:
-			url (str): The URL of the YouTube video to download.
-			output_dir (str, optional): The directory to save the downloaded video.
-					Defaults to "." (current directory).
-
-	Returns:
-			str: The path to the downloaded video file.
-
-	Raises:
-			RuntimeError: If the download process fails.
-	"""
-
-	# Extract the unique YouTube video ID from the URL
-	video_id = get_video_id(youtube_url)
-
-	# Construct a unique filename with video ID and extension
-	output_filename = f"{output_dir}/{video_id}.%(ext)s"
-
-	# Build the youtube-dl command with options for format selection and output filename
-	youtube_dl_cmd = ["yt-dlp",
-										 "-f", "best",  # Download the best available format
-										 "-o", output_filename,  # Specify output filename template
-										 youtube_url]
-
-	# Execute the youtube-dl command
-	try:
-		process = subprocess.run(youtube_dl_cmd, check=True, capture_output=True)
-	except subprocess.CalledProcessError as e:
-		raise RuntimeError(f"youtube-dl download failed: {e.output}") from e
-
-	# Return the path to the downloaded video
-	return output_filename
-
+from helper import download_youtube_video
 
 
 
---
+++ main.py
@@ -0,0 +1,24 @@
+from download_youtube import download_youtube_video
+
+from helper import * 
+
+
+if __name__ == "__main__":
+
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('url',help='youtube url to download')   # positional argument
+	parser.add_argument('-odv', '--output_directory_video',default='videos',help='output directory to save downloaded video')
+	parser.add_argument('-ode', '--output_directory_extracted',default='extracted_frames',help='output directory to save png images files extracted from video')
+	parser.add_argument('-st', '--similarity_threshold',type=float,default='0.8',help='similarity value to check, default 0.8')
+
+
+
+	args = parser.parse_args()
+
+	downloaded_video_path = download_youtube_video(args.url,args.output_directory)
+	print(f"Video downloaded to: {downloaded_video_path}")
+
+	video_to_images(downloaded_video_path
+		,args.output_directory
+		,args.similarity_threshold)
